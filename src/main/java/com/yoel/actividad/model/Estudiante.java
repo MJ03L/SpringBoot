8f48b2162df247d553b678cb1e95996cd484cb5a
Yoel <conversionadev3@gmail.com>
2026-05-16 06:32:06 +0100

    test: añadir tests unitarios para servicio y controlador

--- src/main/java/com/yoel/actividad/model/Estudiante.java
+++ src/main/java/com/yoel/actividad/model/Estudiante.java
@@ -18,4 +18,35 @@ public class Estudiante {
 
     private int edad;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
